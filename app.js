50cbebcf2cdf2403f5f0e35decccc1ecb61a0442
Nicholas Moore <nicholasjosephmoore@gmail.com>
2022-09-13 20:55:02 -0500

    styling changes

---
+++ app.js
@@ -0,0 +1,9 @@
+// Test
+const titleElem = document.querySelector("#board")
+console.log("board" , titleElem);
+
+let restartButton = document.getElementById('restartBtn')
+console.log(restartButton);
+
+let tiles = Array.from(document.getElementsByClassName('tile'))
+console.log(tiles);
