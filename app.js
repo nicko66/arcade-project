0771e8a47cead276712620713866e8c52113296e
Nicholas Moore <nicholasjosephmoore@gmail.com>
2022-09-18 22:30:14 -0500

    minor style, pseduo code, and logic changes

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ console.log("board" , titleElem);
 let wonText = document.getElementById('wonText')
 
 let restartButton = document.getElementById('restartBtn')
-console.log(restartButton);
 
+// array.from converts the html collection into an array
 let tiles = Array.from(document.getElementsByClassName('tile'))
-console.log(tiles);
 
+// create 9 'null' or empty spaces to be filled in
 const spaces = [null, null, null, null, null, null, null, null, null];
 const playerOne = 'O';
 const playerTwo = 'X';
@@ -17,26 +17,20 @@ let currentPlayer = playerTwo
 function gameStart(){
     tiles.forEach(tile => tile.addEventListener('click', tileClicked))
 }
-console.log(tileClicked)
 
+// create a function that allows user to input X or O within gameboard
 function tileClicked(e){
-    const id = e.target.id
+    const id = e.target.id // targets the id within the gameboard
+    
+    
+    if(!spaces[id]){ //allows space to be filled if it doesn't contain an id
+        spaces[id] = currentPlayer // if index is empty it will be filled with currentPlayer which is 'x' or 'o'
+        e.target.innerText = currentPlayer        
+     }
 
-    if(!spaces[id]){
-        spaces[id] = currentPlayer
-        e.target.innerText = currentPlayer
-
-        if(playerWon()!==false){
-            wonText = `${currentPlayer} won!!`
-            let winningTiles = playerWon()
-            
-
-
-        }
-
-        currentPlayer = currentPlayer == playerTwo ? playerOne : playerTwo
+        currentPlayer = currentPlayer == playerTwo ? playerOne : playerTwo // if currentPlayer is = to playerOne, change to playerTwo, if playerTwo, change to playerOne
     }
-}
+
 // all winning combinations to determine winner
 const win = [
  [0,1,2],
@@ -50,10 +44,12 @@ const win = [
 
 ]
 
+// create a function that checks for a winner
 function playerWon(){
+    // loops through the win variable 
     for (const condition of win) {
-        let [a, b, c] = condition
-
+        let [a, b, c] = condition 
+        // checks all spaces for a winning combination, if none is found, return false, or no winner
         if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
             return [a,b,c]
         }
@@ -65,10 +61,11 @@ function playerWon(){
 
 restartButton.addEventListener('click', restart)
 
+// create a function that restarts the game and clears the board.
 function restart(){
-    spaces.fill(null)
+    spaces.fill(null) // clears spaces and fills with null
     tiles.forEach(tile => {
-        tile.innerText = ''
+        tile.innerText = '' // when box is cleared, this fills it with an empty space
     })
     
     wonText = 'Tic Tac Toe'
